418d08189630663cdedca1d26a834cb91402a476
Aymen <you@example.comaymen.neji@esprit.tn>
2022-07-01 18:05:45 +0100

    commentaire crud

--- wolfTech-back/app.js
+++ wolfTech-back/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var commentairesRouter = require('./routes/commentaire');
 var app = express();
 var mongoose = require('mongoose')
 
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use.apply('/commentaires', commentairesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ wolfTech-back/routes/commentaire.js
@@ -0,0 +1,42 @@
+var Commentaire = require('../model/commentaire');
+var express = require('express');
+const commentaire = require('../model/commentaire');
+var router = express.Router();
+//liste des commentaire
+router.get('/', function(req, res){
+    Commentaire.find(function (err, data){
+        if(err) throw err;
+        // res.render("showcontact.twig", {data})
+        res.json(data);
+    });
+});
+
+//Ajouter commentaire 
+router.post('/add', function(req, res){
+    console.log(req.body)
+    var c = new Commentaire ({
+        textC : req.body.textC
+
+    });
+    c.save();
+    res.json(c)
+});
+router.get('/delete/:_id', async(req, res) => {
+    console.log(req.params);
+    await Commentaire.findByIdAndDelete(req.params)
+    res.json('delete success')
+
+});
+router.get('/afficher', async(req, res) => {
+    let commentaire = await Commentaire.find();
+
+    res.json(commentaire)
+});
+router.post('/update/:_id', async(req, res) => {
+    let commentaire = await Commentaire.findById(req.params);
+    commentaire.textC = req.body.textC
+    commentaire.save()
+    res.json(commentaire)
+})
+
+module.exports = router
