6bd964a80dca01808ed4f9af14d7e9dc0e13b62d
RamiK93 <rami.kaabi@esprit.tn>
2022-07-02 16:45:05 +0100

    emailing lors de l'ajout d'un manager et un consultant

--- wolfTech-back/model/user.js
+++ wolfTech-back/model/user.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: { type: String },
   token: { type: String },
-
   role: {
     type: mongoose.Schema.ObjectId,
     ref:'roles' //le nom de la collection dans la bade de donnes 
